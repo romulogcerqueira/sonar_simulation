33539c178367b644bd3afdc23ed5af46e2e70904
Rômulo Cerqueira <romulogcerqueira@gmail.com>
2022-03-25 15:08:51 -0300

    - change container username and hostname - compress build context as gzip - fix bootstrap installation - update autoproj installation - update code style

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@
 . settings.sh
 
 docker build  \
-    --build-arg USERNAME=$USER \
+    --build-arg USERNAME=${CONTAINER_USER} \
     --build-arg UUID=$UUID \
     --build-arg UGID=$UGID \
     -t sonarsim_bionic:devel .
--- docker/run-devel.sh
+++ docker/run-devel.sh
@@ -2,26 +2,26 @@
 
 . settings.sh
 
-if [ ! -d "$SONARSIM_PATH" ]; then
-    mkdir -p "$SONARSIM_PATH"
+if [ ! -d "${SONARSIM_PATH}" ]; then
+    mkdir -p "${SONARSIM_PATH}"
 
-    if [ ! -d "$SONARSIM_PATH" ]; then
+    if [ ! -d "${SONARSIM_PATH}" ]; then
         echo "Could not create Sonarsim's home"
         exit 1
     fi
 fi
 
-if [ ! -d "$SONARSIM_PATH/.ssh" ]; then
-    if [ ! -d "$HOME/.ssh" ]; then
+if [ ! -d "${SONARSIM_PATH}/.ssh" ]; then
+    if [ ! -d "${HOME}/.ssh" ]; then
         printf "Please, setup ssh keys before running the container\n"
 
         exit 1
     fi
-    cp -R "$HOME/.ssh" "$SONARSIM_PATH/"
+    cp -R "${HOME}/.ssh" "${SONARSIM_PATH}/"
 fi
 
-if [ ! -f "$SONARSIM_PATH/.gitconfig" ]; then
-    if [ ! -f "$HOME/.gitconfig" ]; then
+if [ ! -f "${SONARSIM_PATH}/.gitconfig" ]; then
+    if [ ! -f "${HOME}/.gitconfig" ]; then
         printf "Please, setup git before running the container\n\n"
         printf "Use: \n"
         printf "git config --global user.name \"John Doe\"\n"
@@ -29,7 +29,7 @@ if [ ! -f "$SONARSIM_PATH/.gitconfig" ]; then
 
         exit 1
     fi
-    cp "$HOME/.gitconfig" "$SONARSIM_PATH/"
+    cp "${HOME}/.gitconfig" "${SONARSIM_PATH}/"
 fi
 
 XSOCK=/tmp/.X11-unix
@@ -44,11 +44,11 @@ if [ ! "$(docker ps -q -f name=${CONTAINER_NAME}_devel)" ]; then
         echo "Create docker"
         docker create -it \
             --net host \
-            --volume="${SONARSIM_PATH}:${HOME}:rw" \
+            --volume="${SONARSIM_PATH}:/home/${CONTAINER_USER}:rw" \
             --volume="/etc/localtime:/etc/localtime:ro" \
             --env="TERM" \
-            --user="${USER}" \
-            --workdir="/home/${USER}" \
+            --user="${CONTAINER_USER}" \
+            --workdir="/home/${CONTAINER_USER}/sonar_simulation" \
             --name ${CONTAINER_NAME}_devel \
             --privileged \
             --runtime=nvidia \
@@ -56,6 +56,7 @@ if [ ! "$(docker ps -q -f name=${CONTAINER_NAME}_devel)" ]; then
             --volume=${XAUTH}:${XAUTH}:rw \
             --env=XAUTHORITY=${XAUTH} \
             --env=DISPLAY \
+            --hostname ${CONTAINER_HOSTNAME} \
             ${CONTAINER_NAME}:devel
     fi
     echo "Start docker"
